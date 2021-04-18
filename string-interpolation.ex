441a31ef98434d236086691e3cc4013faa24f58e
Jean-Marc McGhee <jeanmarc.mcghee@gmail.com>
2021-04-18 13:56:28 +0100

    Addes string interpolation.

--- string-interpolation.ex
+++ string-interpolation.ex
@@ -1,6 +1,13 @@
 defmodule StringInterpolation do
-    def sample do
+    def interpolate do
         fruit = "apples"
         IO.puts("Favourite fruit is #{fruit}")
     end
+
+    def concatenation do
+        fruit = "apples"
+        phrase = "My favourite fruit is " <> fruit
+        IO.puts(phrase)
+    end
+    
 end
